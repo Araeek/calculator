a080dcf459414452b03aa1ab93bdfdfac080030b
Araeek <aratah.at@gmail.com>
2021-01-01 16:42:55 +0330

    completed all buttons functionality and added keypad support

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const clearButton = document.querySelector("#clear");
 const positiveNegative = document.querySelector("#positive-negative");
 const percentageButton = document.querySelector("#percentage");
 const operators = document.querySelectorAll(".oper");
+const equal = document.querySelector(".equal");
+const point = document.querySelector(".point");
 
 let memory = {
     firstOperand: null,
@@ -12,8 +14,14 @@ let memory = {
     waiting: false,
 };
 
-let displayContent = "";
+let displayContent = "0";
 
+window.addEventListener("keydown",keypad);
+
+function keypad(e) {
+    const key = document.querySelector(`.btn[data-code="${e.keyCode}"]`);
+    key.click();
+}   
 operators.forEach((operation) =>
     operation.addEventListener("click", (e) => {
         saveDisplayContent();
@@ -39,8 +47,40 @@ operators.forEach((operation) =>
     })
 );
 
+equal.addEventListener("click", () => {
+    saveDisplayContent();
+    if (memory["oper"] !== "") {
+        memory["waiting"] = true;
+        updateDisplay(
+            operate(
+                memory["firstOperand"],
+                memory["oper"],
+                memory["secondOperand"]
+            )
+        );
+        memory["firstOperand"] = operate(
+            memory["firstOperand"],
+            memory["oper"],
+            memory["secondOperand"]
+        );
+    }
+    displayContent = "";
+});
+
+point.addEventListener("click", (e) => {
+    if (displayContent.includes(".")) {
+        return;
+    }
+    if (displayContent == "") {
+        displayContent = "0";
+    }
+    displayContent += e.target.textContent;
+    updateDisplay(displayContent);
+});
+
 percentageButton.addEventListener("click", () => {
-    updateDisplay(parseInt(display.textContent) / 100);
+    displayContent = parseFloat(display.textContent) / 100;
+    updateDisplay(displayContent);
 });
 
 positiveNegative.addEventListener("click", () => {
@@ -53,6 +93,9 @@ positiveNegative.addEventListener("click", () => {
 
 numbers.forEach((num) =>
     num.addEventListener("click", (e) => {
+        if (displayContent == "0") {
+            displayContent = "";
+        }
         displayContent += e.target.textContent;
         updateDisplay(displayContent);
     })
@@ -60,10 +103,10 @@ numbers.forEach((num) =>
 
 function saveDisplayContent() {
     if (memory["waiting"] && displayContent != "") {
-        memory["secondOperand"] = parseInt(displayContent);
+        memory["secondOperand"] = parseFloat(displayContent);
         memory["waiting"] = false;
     } else if (displayContent != "") {
-        memory["firstOperand"] = parseInt(displayContent);
+        memory["firstOperand"] = parseFloat(displayContent);
     }
 }
 
@@ -78,7 +121,7 @@ function clearDisplay() {
     memory["secondOperand"] = null;
     memory["oper"] = "";
     memory["waiting"] = false;
-    displayContent = "";
+    displayContent = "0";
     updateDisplay("0");
 }
 
@@ -98,11 +141,11 @@ function operate(firstNumber, operator, secondNumber) {
 }
 
 function add(a, b) {
-    return a + b;
+    return (a * 10000000000 + b * 10000000000) / 10000000000;
 }
 
 function subtract(a, b) {
-    return a - b;
+    return (a * 10000000000 - b * 10000000000) / 10000000000;
 }
 
 function multiply(a, b) {
@@ -110,5 +153,8 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    if (b == 0) {
+        return "Error";
+    }
     return a / b;
 }
